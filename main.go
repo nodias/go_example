0c4e2f371e801fa04628425c5970c2593ac065c4
nodias <nodias@naver.com>
2019-05-23 17:51:12 +0900

    start web framework example

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/unrolled/render"
+import (
+	"net/http"
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
+)
 
 var renderer *render.Render
 
@@ -10,6 +16,18 @@ func init() {
 }
 
 func main() {
-	router := httprouter.New()
+	//라우터 생성
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		renderer.HTML(w, http.StatusOK, "index", map[string]string{"title": "Simple Chat!"})
+	}).Methods("GET")
 
+	//negroni 미들웨어 생성
+	n := negroni.Classic()
+
+	//negroni에 router를 핸들러로 등록
+	n.UseHandler(router)
+
+	//웹서버 실행
+	n.Run(":3000")
 }
